006574819b3b9b34ea5657efde4db1bdd606ca91
hamitmizrak <hamitmizrak@gmail.com>
2023-06-24 10:16:36 +0300

    Template Added

--- src/main/java/com/hamitmizrak/SpringBoot9GroupApplication.java
+++ src/main/java/com/hamitmizrak/SpringBoot9GroupApplication.java
@@ -1,12 +1,43 @@
 package com.hamitmizrak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+
+// Spring Security
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+/*@SpringBootApplication*/
 public class SpringBoot9GroupApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
+
 	public static void main(String[] args) {
+
+		// devtools active isActive
+		//System.setProperty("spring.devtools.restart.enabled","true");
+
+		// PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+		//Disables headless JOptionPane
+		System.setProperty("java.awt.headless", "false");
+
 		SpringApplication.run(SpringBoot9GroupApplication.class, args);
 	}
 
