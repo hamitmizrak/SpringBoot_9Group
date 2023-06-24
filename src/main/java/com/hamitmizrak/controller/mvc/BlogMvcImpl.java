4046592687bd46346b4fe74d5273e50f2070d8c3
hamitmizrak <hamitmizrak@gmail.com>
2023-06-24 12:20:26 +0300

    Spring MVC data

--- src/main/java/com/hamitmizrak/controller/mvc/BlogMvcImpl.java
+++ src/main/java/com/hamitmizrak/controller/mvc/BlogMvcImpl.java
@@ -1,6 +1,10 @@
 package com.hamitmizrak.controller.mvc;
 
 import com.hamitmizrak.business.dto.BlogDto;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 public  class BlogMvcImpl implements IBlogMvc<BlogDto> {
@@ -27,5 +31,37 @@ public  class BlogMvcImpl implements IBlogMvc<BlogDto> {
     @Override
     public List<BlogDto> list() {
         return null;
+    }
+
+    // Tutorials
+    // http://localhost:2222/getdata0
+    @GetMapping("/getdata0")
+    @ResponseBody
+    public String getThymeleaf0(){
+        return "Sayfasız data";
+    }
+
+    // http://localhost:2222/getdata1
+    @GetMapping("/getdata1")
+    public String getThymeleaf1(){
+        return "page1";
+    }
+
+    // http://localhost:2222/getdata2
+    @GetMapping("/getdata2")
+    public String getThymeleaf2(Model model){
+        model.addAttribute("key2","String data");
+        return "page2";
+    }
+
+    // http://localhost:2222/getdata2
+    @GetMapping("/getdata3")
+    public String getThymeleaf3(Model model){
+        BlogDto blogDto=new BlogDto();
+        blogDto.setId(1L);
+        blogDto.setHeader("Header");
+        blogDto.setContent("Content");
+        model.addAttribute("key3",blogDto);
+        return "page3";
     }
 }
