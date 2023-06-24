4acd67be28214a467083d22bbde88ca505f492a6
hamitmizrak <hamitmizrak@gmail.com>
2023-06-24 12:34:01 +0300

    Spring MVC continues ...

--- src/main/java/com/hamitmizrak/controller/mvc/BlogMvcImpl.java
+++ src/main/java/com/hamitmizrak/controller/mvc/BlogMvcImpl.java
@@ -1,12 +1,14 @@
 package com.hamitmizrak.controller.mvc;
 
 import com.hamitmizrak.business.dto.BlogDto;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Controller
 public  class BlogMvcImpl implements IBlogMvc<BlogDto> {
 
     // CREATE
@@ -34,27 +36,27 @@ public  class BlogMvcImpl implements IBlogMvc<BlogDto> {
     }
 
     // Tutorials
-    // http://localhost:2222/getdata0
-    @GetMapping("/getdata0")
+    // http://localhost:4444/getData0
+    @GetMapping("/getData0")
     @ResponseBody
     public String getThymeleaf0(){
         return "Sayfasız data";
     }
 
-    // http://localhost:2222/getData1
+    // http://localhost:4444/getData1
     @GetMapping("/getData1")
     public String getThymeleaf1(){
         return "page1";
     }
 
-    // http://localhost:2222/getData2
+    // http://localhost:4444/getData2
     @GetMapping("/getData2")
     public String getThymeleaf2(Model model){
         model.addAttribute("key2","String data");
         return "page2";
     }
 
-    // http://localhost:2222/getData3
+    // http://localhost:4444/getData3
     @GetMapping("/getData3")
     public String getThymeleaf3(Model model){
         BlogDto blogDto=new BlogDto();
