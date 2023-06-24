325dd987ac5b3786da68ce6003c90938e6074138
hamitmizrak <hamitmizrak@gmail.com>
2023-06-24 10:06:46 +0300

    Base Template

---
+++ src/main/java/com/hamitmizrak/SpringBoot9GroupApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot9GroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot9GroupApplication.class, args);
+	}
+
+}
