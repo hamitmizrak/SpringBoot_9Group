bce19b663687677e7985311a8ee7d6739cc95d37
hamitmizrak <hamitmizrak@gmail.com>
2023-06-25 09:57:44 +0300

    H2-console

--- src/main/java/com/hamitmizrak/data/repository/IBlogRepository.java
+++ src/main/java/com/hamitmizrak/data/repository/IBlogRepository.java
@@ -4,6 +4,7 @@ import com.hamitmizrak.data.entity.BlogEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// http://localhost:2222/h2-console
 @Repository
 public interface IBlogRepository extends CrudRepository<BlogEntity,Long> {
 }
